01be15948864729f902ca4944da9ebde5f314f4a
albi-c <albert.csontos@gmail.com>
2025-12-02 15:19:51 +0100

    2016 17-18

--- src/y2016/day17.rs
+++ src/y2016/day17.rs
@@ -2,10 +2,58 @@ use advent::prelude::*;
 
 parse!();
 
+fn move_options(salt: &str, (x, y): (usize, usize), path: &str) -> impl Iterator<Item = (u8, (usize, usize))> + use<> {
+    let mut hash = [0; 32];
+    hex::encode_to_slice(md5::compute(format!("{salt}{path}")).0, &mut hash).unwrap();
+    [
+        (b'U', (0isize, -1isize)),
+        (b'D', (0, 1)),
+        (b'L', (-1, 0)),
+        (b'R', (1, 0)),
+    ].into_iter()
+        .zip([hash[0], hash[1], hash[2], hash[3]].into_iter())
+        .filter_map(move |((d, (dx, dy)), h)| (h >= b'b' && h <= b'f')
+            .then(move || x.checked_add_signed(dx)
+                .and_then(move |x| y.checked_add_signed(dy)
+                    .map(move |y| (d, (x, y)))))
+            .flatten())
+        .filter(move |(_, (x, y))| *x < 4 && *y < 4)
+}
+
 pub fn part1(input: Input) -> impl Display {
-    0
+    for (_, (path, pos)) in bfs(("".to_owned(), (0, 0)), move |(path, pos)| {
+        move_options(input, pos, &path)
+            .map(move |(ch, new_pos)| {
+                let mut new_path = path.clone();
+                new_path.push(ch as char);
+                (new_path, new_pos)
+            })
+    }) {
+        if pos == (3, 3) {
+            return path;
+        }
+    }
+    "".to_string()
 }
 
 pub fn part2(input: Input) -> impl Display {
     0
+    // let mut longest = 0;
+    // for (dist, (_, pos)) in bfs(("".to_owned(), (0, 0)), move |(path, pos)| {
+    //     move_options(input, pos, &path)
+    //         .map(move |(ch, new_pos)| {
+    //             let mut new_path = path.clone();
+    //             new_path.push(ch as char);
+    //             (new_path, new_pos)
+    //         })
+    // }) {
+    //     if pos == (3, 3) {
+    //         let prev = longest;
+    //         longest = longest.max(dist);
+    //         if longest != prev {
+    //             println!("17:2 - longest {longest}");
+    //         }
+    //     }
+    // }
+    // unreachable!()
 }
--- src/y2016/day18.rs
+++ src/y2016/day18.rs
@@ -1,11 +1,26 @@
 use advent::prelude::*;
 
-parse!();
+parse!(Vec<bool>, { '^' => true, '.' => false }+);
+
+fn next_row(row: &[bool]) -> Vec<bool> {
+    (0..row.len())
+        .map(|i| i.checked_sub(1).map_or(false, |i| row[i]) != *row.get(i + 1).unwrap_or(&false))
+        .collect_vec()
+}
+
+fn solve(mut row: Vec<bool>, rows: usize) -> usize {
+    let mut result = 0;
+    for _ in 0..rows {
+        result += row.iter().filter(|&&x| !x).count();
+        row = next_row(&row);
+    }
+    result
+}
 
 pub fn part1(input: Input) -> impl Display {
-    0
+    solve(input, 40)
 }
 
 pub fn part2(input: Input) -> impl Display {
-    0
+    solve(input, 400000)
 }
