7fbd292515deba810f4d72a9c094c1c528634522
albi-c <albert.csontos@gmail.com>
2025-12-02 18:04:02 +0100

    2016 d20

--- src/y2016/day20.rs
+++ src/y2016/day20.rs
@@ -1,11 +1,17 @@
 use advent::prelude::*;
 
-parse!();
+parse!(Vec<(u32, u32)>, lines(u32 '-' u32));
 
 pub fn part1(input: Input) -> impl Display {
-    0
+    (0u32..=u32::MAX)
+        .into_par_iter()
+        .find_first(move |&i| input.iter().all(|&(lo, hi)| i < lo || i > hi))
+        .unwrap()
 }
 
 pub fn part2(input: Input) -> impl Display {
-    0
+    (0u32..=u32::MAX)
+        .into_par_iter()
+        .filter(move |&i| input.iter().all(|&(lo, hi)| i < lo || i > hi))
+        .count()
 }
