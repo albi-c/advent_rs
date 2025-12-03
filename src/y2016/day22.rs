af2e5ea90c67976d8f6aa0e0e74b956dc380b5e0
albi-c <albert.csontos@gmail.com>
2025-12-03 09:07:39 +0100

    2016 22p1, 2025 3

--- src/y2016/day22.rs
+++ src/y2016/day22.rs
@@ -1,11 +1,53 @@
 use advent::prelude::*;
 
-parse!();
+// input is edited to remove first two lines
+// root@ebhq-gridcenter# df -h
+// Filesystem              Size  Used  Avail  Use%
+parse!(Vec<(usize, usize, usize, usize)>, lines( "/dev/grid/node-x" x:usize "-y" y:usize " "+ s:usize "T" " "+ u:usize "T" " "+ usize "T" " "+ usize "%" => (x, y, s, u)));
 
-pub fn part1(_input: Input) -> impl Display {
-    0
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+struct Node {
+    pub size: usize,
+    pub used: usize,
 }
 
-pub fn part2(_input: Input) -> impl Display {
-    0
+fn make_grid(input: Input) -> Grid2<Node> {
+    let width = input.iter().map(|n| n.0).max().unwrap() + 1;
+    let height = input.iter().map(|n| n.1).max().unwrap() + 1;
+    let mut grid = Grid2::new_value((width, height), None);
+    for (x, y, size, used) in input {
+        let prev = grid[(x, y)].replace(Node { size, used });
+        assert!(prev.is_none());
+    }
+    grid.into_map(|node| node.expect("node not found"))
+}
+
+pub fn part1(input: Input) -> impl Display {
+    let grid = make_grid(input);
+    grid
+        .enumerate()
+        .filter(|&(_, a)| a.used != 0)
+        .flat_map(|(ap, a)| grid
+            .enumerate()
+            .filter(move |&(bp, b)| ap != bp && a.used + b.used <= b.size))
+        .count()
+}
+
+pub fn part2(input: Input) -> impl Display {
+    let grid = make_grid(input);
+    grid
+        .enumerate()
+        .filter(|&(_, a)| a.used != 0)
+        .flat_map(|(ap, a)| grid
+            .neighbors(ap, false)
+            .filter(move |&(bp, b)| ap != bp && a.used + b.used <= b.size))
+        .count()
+    // let grid = make_grid(input);
+    // let data_pos = (grid.width() - 1, 0);
+    // for (dist, (_, pos)) in bfs((grid, data_pos), |(grid, data_pos)| {}) {
+    //     if pos == data_pos {
+    //
+    //     }
+    // }
+    // 0
 }
--- src/y2025/day3.rs
+++ src/y2025/day3.rs
@@ -1,11 +1,28 @@
 use advent::prelude::*;
 
-parse!();
+parse!(Vec<Vec<usize>>, lines(digit+));
 
-pub fn part1(_input: Input) -> impl Display {
-    0
+fn find_largest(row: &[usize], n: usize) -> usize {
+    let mut result = 0;
+    let mut index = 0;
+    for i in 0..n {
+        let remaining = n - i - 1;
+        let (j, m) = row[index..row.len()-remaining].iter().enumerate().max_by_key(|&(j, x)| (*x, -(j as isize))).unwrap();
+        result *= 10;
+        result += *m;
+        index += j + 1;
+    }
+    result
 }
 
-pub fn part2(_input: Input) -> impl Display {
-    0
+pub fn part1(input: Input) -> impl Display {
+    input.into_iter()
+        .map(|row| find_largest(&row, 2))
+        .sum::<usize>()
+}
+
+pub fn part2(input: Input) -> impl Display {
+    input.into_iter()
+        .map(|row| find_largest(&row, 12))
+        .sum::<usize>()
 }
