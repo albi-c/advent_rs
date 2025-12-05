368df086c52e70f645b3e2c295ec53d3388c9762
albi-c <albert.csontos@gmail.com>
2025-12-05 09:14:14 +0100

    2025 5 using library functions

--- src/y2025/day5.rs
+++ src/y2025/day5.rs
@@ -1,37 +1,22 @@
+use advent::intervals::IntervalInclusive;
 use advent::prelude::*;
 
-parse!((Vec<(usize, usize)>, Vec<usize>), section(lines(usize '-' usize)) section(lines(usize)));
+parse!((Vec<IntervalInclusive>, Vec<usize>), section(lines(interval_inclusive_us('-'))) section(lines(usize)));
 
 pub fn part1(input: Input) -> impl Display {
     let (ranges, ids) = input;
     ids
         .into_iter()
         .filter(|&i| ranges.iter()
-            .any(|&(lo, hi)| i >= lo && i <= hi))
+            .any(|&interval| interval.contains(i)))
         .count()
-}
-
-fn join_intervals(a: (usize, usize), b: (usize, usize)) -> Option<(usize, usize)> {
-    let (al, ah) = a;
-    let (bl, bh) = b;
-    if ah < bl {
-        None
-    } else {
-        Some((al.min(bl), ah.max(bh)))
-    }
 }
 
 pub fn part2(input: Input) -> impl Display {
     let (mut ranges, _) = input;
-    ranges.sort();
-    for i in (1..ranges.len()).rev() {
-        if let Some(interval) = join_intervals(ranges[i - 1], ranges[i]) {
-            ranges[i - 1] = interval;
-            ranges.remove(i);
-        }
-    }
+    IntervalInclusive::optimize_vec(&mut ranges);
     ranges
         .into_iter()
-        .map(|(a, b)| b - a + 1)
+        .map(IntervalInclusive::len)
         .sum::<usize>()
 }
