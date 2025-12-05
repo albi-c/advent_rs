cfa52ee00bb238f06b71969a4e096b415b7d7f5e
albi-c <albert.csontos@gmail.com>
2025-12-05 09:45:49 +0100

    2016 20 optimize

--- src/y2016/day20.rs
+++ src/y2016/day20.rs
@@ -1,17 +1,25 @@
+use advent::intervals::IntervalInclusive;
 use advent::prelude::*;
 
-parse!(Vec<(u32, u32)>, lines(u32 '-' u32));
+parse!(Vec<IntervalInclusive<u32>>, lines(interval_inclusive(u32 '-' u32)));
 
-pub fn part1(input: Input) -> impl Display {
-    (0u32..=u32::MAX)
-        .into_par_iter()
-        .find_first(move |&i| input.iter().all(|&(lo, hi)| i < lo || i > hi))
-        .unwrap()
+pub fn part1(mut input: Input) -> impl Display {
+    IntervalInclusive::optimize_vec(&mut input);
+    input[0].1 + 1
 }
 
-pub fn part2(input: Input) -> impl Display {
-    (0u32..=u32::MAX)
-        .into_par_iter()
-        .filter(move |&i| input.iter().all(|&(lo, hi)| i < lo || i > hi))
-        .count()
+pub fn part2(mut input: Input) -> impl Display {
+    IntervalInclusive::optimize_vec(&mut input);
+    let mut start = Some(0);
+    let mut result = 0;
+    for interval in input {
+        if let Some(start) = start {
+            result += interval.0 - start;
+        }
+        start = interval.1.checked_add(1);
+    }
+    if let Some(start) = start {
+        result += u32::MAX - start;
+    }
+    result
 }
