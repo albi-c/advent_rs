21aad47ff4c7e93b17f7cddee4f18ed257b7fc61
albi-c <albert.csontos@gmail.com>
2025-12-09 16:58:48 +0100

    2025 9 use coordinate compression utility

--- src/y2025/day9.rs
+++ src/y2025/day9.rs
@@ -1,5 +1,5 @@
 use advent::prelude::*;
-use advent::prelude::ordered_compression::{OrderedCompression, OrderedCompressionKey};
+use advent::prelude::ordered_compression::CoordinateCompression;
 
 parse!(Vec<usizex2>, lines({ x:usize ',' y:usize => usizex2::from_array([x, y]) }));
 
@@ -12,12 +12,10 @@ pub fn part1(input: Input) -> impl Display {
 }
 
 pub fn part2(input: Input) -> impl Display {
-    let (x_coords, y_coords) = input.iter().map(|&p| (p[0], p[1])).unzip_to_vec();
-    let x_coords = OrderedCompression::from_vec(x_coords);
-    let y_coords = OrderedCompression::from_vec(y_coords);
+    let coord_comp = CoordinateCompression::from_iter(input.iter().copied());
 
     let compressed = input.iter()
-        .map(|&p| usizex2::from_array([x_coords.to_key(&p[0]).0, y_coords.to_key(&p[1]).0]))
+        .map(|&p| usizex2::from_array(coord_comp.compress(p)))
         .collect_vec();
     let size = compressed.iter().copied().reduce(|a, b| a.simd_max(b)).unwrap() + usizex2::splat(1);
 
@@ -40,14 +38,8 @@ pub fn part2(input: Input) -> impl Display {
                 .all(|p| grid.slice_range(&p).iter().all(|&x| x))
         })
         .map(|(&a, &b)| { (
-            usizex2::from_array([
-                *x_coords.to_val(OrderedCompressionKey(a[0])),
-                *y_coords.to_val(OrderedCompressionKey(a[1])),
-            ]),
-            usizex2::from_array([
-                *x_coords.to_val(OrderedCompressionKey(b[0])),
-                *y_coords.to_val(OrderedCompressionKey(b[1])),
-            ]),
+            usizex2::from_array(coord_comp.decompress(a)),
+            usizex2::from_array(coord_comp.decompress(b)),
         ) })
         .map(|(a, b)| (a.abs_diff(b) + usizex2::splat(1)).reduce_product())
         .max()
