a4b087bc7cf35f8bbd6837c803433eb378fe1887
albi-c <albert.csontos@gmail.com>
2025-12-11 12:47:47 +0100

    2016 19

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![feature(portable_simd)]
 #![feature(iter_map_windows)]
+#![feature(linked_list_cursors)]
+#![feature(btree_cursors)]
 
 mod y2016;
 mod y2025;
--- src/y2016/day19.rs
+++ src/y2016/day19.rs
@@ -1,11 +1,34 @@
 use advent::prelude::*;
 
-parse!();
+parse!(u32, u32);
 
-pub fn part1(_input: Input) -> impl Display {
-    0
+pub fn part1(input: Input) -> impl Display {
+    let mut list = (1..=input).collect_vec();
+    let mut i = 0;
+    while list.len() > 1 {
+        list.retain(|_| {
+            i += 1;
+            i % 2 == 1
+        });
+    }
+    list[0]
 }
 
-pub fn part2(_input: Input) -> impl Display {
-    0
+pub fn part2(input: Input) -> impl Display {
+    let mut list = (1..=input).collect::<indexset::BTreeSet<_>>();
+    let mut cur = 0;
+    while list.len() > 1 {
+        let Some(&n) = list.lower_bound(&(cur + 1)) else {
+            cur = 0;
+            continue;
+        };
+        let idx = list.rank(&n);
+        list.pop_index((idx + list.len() / 2) % list.len());
+        if idx >= list.len() {
+            cur = 0;
+        } else {
+            cur = n;
+        }
+    }
+    list.pop_first().unwrap()
 }
